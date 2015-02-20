7b848d8f0fb8dee5ecab645fc69ac22a418bb35a
Benji Dalton <daylightsavings@gmail.com>
2015-02-21 04:02:22 +1300

    removes frontend as submodule, adds to main repo

---
+++ frontend/app/initializers/session.js
@@ -0,0 +1,23 @@
+export function initialize(/* container, application */) {
+  // application.inject('route', 'foo', 'service:foo');
+}
+
+import Ember from 'ember';
+
+export default {
+  name: 'session',
+  initialize: function(container) {
+    Ember.$.ajax({
+      url: '/api/sessions/current',
+      type: 'GET',
+      dataType: "json",
+      success:function(data) {
+        container.lookup('controller:session').set('currentUser', {
+          email: data['email'],
+          name: data['name'],
+          id: data['id']
+        });
+      },
+    });
+  }
+};
