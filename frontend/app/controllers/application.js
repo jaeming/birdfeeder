e22aa2e1b92a8d7211998624862a1371cd817904
Benji Dalton <daylightsavings@gmail.com>
2015-03-01 07:49:13 +1300

    adds mobile slide menu panel

--- frontend/app/controllers/application.js
+++ frontend/app/controllers/application.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  needs: ['session'],
+  needs: ['session', 'index'],
 	actionsVisible: false,
+	accountVisible: false,
+	smallLogo: false,
 	actions: {
 		optionsShow: function() {
 			var _this = this;
@@ -10,6 +12,12 @@ export default Ember.Controller.extend({
 			Ember.$('.account-box').mouseleave(function() {
 				_this.set('actionsVisible', false);
 			});
+		},
+		showSidePanel: function() {
+			this.get('controllers.index').toggleProperty('sideVisible');
+			this.get('controllers.index').toggleProperty('slidePanel');
+			this.toggleProperty('accountVisible');
+			this.toggleProperty('smallLogo');
 		}
 	}
 });
--- frontend/app/controllers/index.js
+++ frontend/app/controllers/index.js
@@ -1,15 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	needs: ['application'],
 	scrollVisible: false,
+	sideVisible: false,
+	slidePanel: false,
 	actions: {
 		showScrollbar: function() {
 		var _this = this;
 		this.set('scrollVisible', true);
 		Ember.$('.side-bar').mouseleave(function() {
-			console.log("exit sidebar");
 			_this.set('scrollVisible', false);
 		});
-	}
+		},
+		closeSidePanel: function() {
+			this.toggleProperty('sideVisible', false);
+			this.set('slidePanel', false);
+			this.get('controllers.application').set('accountVisible', false);
+			this.get('controllers.application').set('smallLogo', false);
+		}
 	}
 });
