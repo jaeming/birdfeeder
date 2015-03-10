a13aec9937c7345e6dac0c4575a2047c8607df75
Benji Dalton <daylightsavings@gmail.com>
2015-03-11 00:36:06 +1300

    adds travis ci

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email, :password, :password_conformation, :name)
+      params.require(:user).permit(:email, :password, :password_confirmation, :name)
     end
 
 end
