3b2eb41eae3faea35a5e79e713901d542657883e
Benji Dalton <daylightsavings@gmail.com>
2015-04-09 03:55:19 +1200

    fixes devise confirmation path and error handling

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,24 @@
 class ConfirmationsController < Devise::ConfirmationsController
 
+  def new
+    super
+  end
+
+  def create
+    super
+  end
+
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+
+    if resource.errors.empty?
+      sign_in(resource_name, resource)
+      redirect_to root_path
+    else
+      respond_with resource.errors
+    end
+  end
+
   private
     def after_confirmation_path_for(resource_name, resource)
       redirect_to root_path
