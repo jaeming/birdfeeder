fa6448aa055e857e16ae9f5e32ed9223c0340034
Benji Dalton <daylightsavings@gmail.com>
2015-06-19 01:40:46 +1200

    Revert "removes some unecessarily generated test unit files"
    This reverts commit 7a87179f977660f3e7fa979d5fa7c0b84c79702e.

---
+++ frontend/tests/unit/helpers/autocomplete-test.js
@@ -0,0 +1,11 @@
+import {
+  autocomplete
+} from 'frontend/helpers/autocomplete';
+
+module('AutocompleteHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = autocomplete(42);
+  ok(result);
+});
